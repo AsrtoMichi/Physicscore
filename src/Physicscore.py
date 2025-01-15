50aec8f058ad966493b55a9b14dd8d1003e46a51
AsrtoMichi <146475341+AsrtoMichi@users.noreply.github.com>
2025-01-15 14:56:04 +0100

    Update Physicscore.py

--- src/Physicscore.py
+++ src/Physicscore.py
@@ -36,7 +36,6 @@ class Main(Tk):
     def __init__(self):
         super().__init__()
 
-
         Label(self, text="v0.1.0.0\nCopyright (C) 2024 AsrtoMichi", justify='right').pack(
             side="bottom", anchor="e", padx=8, pady=8
         )
