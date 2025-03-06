db7fd2c9305235fa7e95febf81974e5d45aa2acd
AsrtoMichi <asrtomichi@gmail.com>
2025-03-06 22:11:06 +0100

    Update update_readme.py

--- .github/workflows/update_readme.py
+++ .github/workflows/update_readme.py
@@ -1,29 +1,12 @@
 import json
 import os
 
-# Percorsi dei file
-json_file_path = 'path/to/your/config.json'
-readme_file_path = 'README.md'
-temp_readme_file_path = 'README_temp.md'
 
-# Leggi il contenuto del file JSON
-with open(json_file_path, 'r') as json_file:
-    json_content = json.load(json_file)
+json_file_path = '/home/runner/work/Physicscore/Physicscore/Template.json'
+readme_file_path = '/home/runner/work/Physicscore/Physicscore/README.md'
 
-# Converti il contenuto del file JSON in una stringa formattata
-json_string = json.dumps(json_content, indent=4)
+json_string = json.dumps(json.load(open(json_file_path, 'r')), indent=4)
 
-# Leggi il contenuto del README.md originale
-with open(readme_file_path, 'r') as readme_file:
-    readme_content = readme_file.read()
+updated_readme_content = open(readme_file_path, 'r').read().replace('<!-- INSERT JSON HERE -->', f'```json\n{json_string}\n```')
 
-# Inserisci il contenuto del file JSON nel README.md
-placeholder = '<!-- INSERT JSON HERE -->'
-updated_readme_content = readme_content.replace(placeholder, f'```json\n{json_string}\n```')
-
-# Scrivi il contenuto aggiornato in un file temporaneo
-with open(temp_readme_file_path, 'w') as temp_readme_file:
-    temp_readme_file.write(updated_readme_content)
-
-# Sostituisci il file README.md originale con il file temporaneo
-os.replace(temp_readme_file_path, readme_file_path)
+open(readme_file_path, 'w').write(updated_readme_content)
