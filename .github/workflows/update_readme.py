20631fcb33d29a1517c119010b7bf7353ba9a144
AsrtoMichi <asrtomichi@gmail.com>
2025-03-06 22:19:16 +0100

    Update update_readme.py

--- .github/workflows/update_readme.py
+++ .github/workflows/update_readme.py
@@ -2,19 +2,6 @@ import json
 import os
 
 
-def display_tree(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print(f'{indent}{os.path.basename(root)}/')
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print(f'{subindent}{f}')
-
-# Replace 'your_directory_path' with the path of the directory you want to display
-display_tree('your_directory_path')
-
-
 json_file_path = '/home/runner/work/Physicscore/Template.json'
 readme_file_path = '/home/runner/work/Physicscore/README.md'
 
