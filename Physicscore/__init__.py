cf2e5c978357cf6bfde79127662ac056215ddc3b
AsrtoMichi <asrtomichi@gmail.com>
2025-05-08 11:14:28 +0200

    Delete Physicscore directory

--- Physicscore/__init__.py
+++
@@ -1,9 +0,0 @@
-from Physicscore.src import generate_report, Physicscore
-
-__all__ = ["physicscore", "reportgen"]
-
-def physicscore():
-    Physicscore().mainloop()
-
-def reportgen():
-    generate_report()
